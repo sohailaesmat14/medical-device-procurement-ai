f7abb05259bca1392a8c3e0588ee55e115327d84
sohailaesmat14 <sohailabs1410@gmail.com>
2026-08-10 08:52:17 +0300

    add composite indexes to Standards, VendorOffers, and OfferEvaluations using Fluent API to eliminate full table scans on frequent queries

--- MediTender.API/Data/ApplicationDbContext.cs
+++ MediTender.API/Data/ApplicationDbContext.cs
@@ -16,5 +16,22 @@ namespace MediTender.API.Data
         public DbSet<OfferEvaluation> OfferEvaluations { get; set; }
         public DbSet<EvaluationDetail> EvaluationDetails { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Standard>()
+                .HasIndex(s => s.TenderId)
+                .HasDatabaseName("IX_Standards_TenderId");
+
+            modelBuilder.Entity<VendorOffer>()
+                .HasIndex(v => new { v.TenderId, v.CompanyName })
+                .HasDatabaseName("IX_VendorOffers_TenderId_CompanyName");
+
+            modelBuilder.Entity<OfferEvaluation>()
+                .HasIndex(e => new { e.TenderId, e.VendorName })
+                .HasDatabaseName("IX_OfferEvaluations_TenderId_VendorName");
+        }
     }
 }
