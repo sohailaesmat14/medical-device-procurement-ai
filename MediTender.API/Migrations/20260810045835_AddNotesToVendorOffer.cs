7e4578239e471d677422eb655ac9a1de569a6318
sohaila esmat <sohailabs1410@gmail.com>
2026-08-10 08:02:05 +0300

    add Notes migration

---
+++ MediTender.API/Migrations/20260810045835_AddNotesToVendorOffer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediTender.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotesToVendorOffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "VendorOffers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "VendorOffers");
+        }
+    }
+}
