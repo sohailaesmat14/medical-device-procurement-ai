1cb01d6bb9489fc44c05374663afcf0d322bf268
sohaila esmat <sohailabs1410@gmail.com>
2026-08-12 14:51:19 +0300

    implement global fetch interceptor to gracefully handle 401 token expiration and redirect to login page

---
+++ MediTender.API/Migrations/20260812110315_v6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediTender.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedVerificationAttempts",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedVerificationAttempts",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
